df8c593a7e92a17cbaad6d0deaee4e05fff08d37
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-11-11 16:08:21 -0500

    Update NS_Muon.py

--- optimizers/NS_Muon.py
+++ optimizers/NS_Muon.py
@@ -20,8 +20,8 @@ def zeropower_via_newtonschulz5(G, steps=5):
     return X.type_as(G)
 
 class Muon(torch.optim.Optimizer):
-    def __init__(self, params, lr=0.02, weight_decay=0.01, momentum=0.95):
-        defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum)
+    def __init__(self,params,lr=0.001, weight_decay=0.01, momentum=0.95, compressor=NoneCompressor(),device="cpu",devices=[],comm_set=['x'],lr_decay="none",nvlink=False):
+        defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum, compressor=NoneCompressor(), device=device, devices=devices,comm_set=comm_set,lr_decay=lr_decay,nvlink=nvlink)
         super().__init__(params, defaults)
     
     @torch.no_grad()
