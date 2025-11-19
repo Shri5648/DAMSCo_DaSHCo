5054ee9d42ddf457e636dd9aee75fb54ceb5ad8b
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-11-19 09:43:45 -0500

    Update DistDataModel.py

--- src/DistDataModel.py
+++ src/DistDataModel.py
@@ -116,7 +116,7 @@ class DistDataModel():
 			if nvlink:
 				nvlink = nvlink_check()
 
-			if (optimizer_name=='NSMuon') or (optimizer_name=='ESMuonpnorm'):
+			if (optimizer_name=='NSMuon') or (optimizer_name=='ESMuonpnorm') or (optimizer_name=='ESMuonqnorm') or (optimizer_name=='ESMuon'):
 				hidden_matrix_params = [p for p in self.model.parameters() if p.ndim >= 2]
 				other_params = [p for p in self.model.parameters() if p.ndim < 2]           # Biases, BatchNorm params
 				optim1 = get_optimizer(optimizer_name,hidden_matrix_params, compressor=compressor, \
