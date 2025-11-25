6318492f4fd694d0995fe5e9283e9520cdb5c884
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-11-25 18:08:49 -0500

    Update train_fineweb_config.py

--- config/train_fineweb_config.py
+++ config/train_fineweb_config.py
@@ -3,3 +3,4 @@ vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens
 n_layer: int = 12 # number of layers
 n_head: int = 12 # number of heads
 n_embd: int = 768 # embedding dimension
+dropout=0.1
