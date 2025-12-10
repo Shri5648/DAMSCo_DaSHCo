bd756eee1e14c1135c22088b6e715e89a27c55de
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-12-10 08:12:34 -0500

    Comment out AdamW optimizer assignment
    Comment out the AdamW optimizer assignment for clarity.

--- src/DistDataModel.py
+++ src/DistDataModel.py
@@ -145,8 +145,8 @@ class DistDataModel():
 				optim2 = torch.optim.AdamW(other_params, lr=0.008, betas=(0.65, 0.95), eps=1e-8, weight_decay=0.0)				
 				self.optim=[optim1, optim2]
 
-			elif (optimizer_name=='AdamW'):
-				self.optim = torch.optim.AdamW(self.model.parameters(), lr=0.008, betas=(0.65, 0.95), eps=1e-8, weight_decay=0.0)
+			#elif (optimizer_name=='AdamW'):
+			#	self.optim = torch.optim.AdamW(self.model.parameters(), lr=0.008, betas=(0.65, 0.95), eps=1e-8, weight_decay=0.0)
 			
 			else:
 				self.optim = get_optimizer(optimizer_name,self.model, compressor=compressor, \
