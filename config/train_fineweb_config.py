44c02329174ca1348f0acde93af8aa96c9507f5c
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-12-10 10:27:35 -0500

    Update train_fineweb_config.py

--- config/train_fineweb_config.py
+++ config/train_fineweb_config.py
@@ -5,5 +5,12 @@ n_layer: int = 12 # number of layers
 n_head: int = 12 # number of heads
 n_embd: int = 768 # embedding dimension
 dropout=0.1
-gradient_accumulation_steps = 1
-batch_size = 16
+
+batch_size = 524288 # 2**19, ~0.5M, in number of tokens
+B = 64 # micro batch size
+T = 1024 # sequence length
+assert batch_size % (B * T * ddp_world_size) == 0, "make sure total_batch_size is divisible by B * T * ddp_world_size"
+gradient_accumulation_steps = batch_size // (B * T * ddp_world_size)
+if master_process:
+    print(f"total desired batch size: {batch_size}")
+    print(f"=> calculated gradient accumulation steps: {gradient_accumulation_steps}")
