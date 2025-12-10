5099fa66f1d5ade63709c394abbcd9bd2c12c26e
Shreenidhi Srinivasan <156630615+Shri5648@users.noreply.github.com>
2025-12-10 12:07:41 -0500

    Refactor train method in DistDataModel
    Refactor training method for improved clarity and functionality.

--- src/DistDataModel.py
+++ src/DistDataModel.py
@@ -178,202 +178,202 @@ class DistDataModel():
 			else:
 				self.device = self.optim.devices[rank % len(self.optim.devices)]
 
-	# '''
-	# Performs training for the defined number of epochs.
-	# '''
-	# def train(self,output_file="default",verbose=False):
-	# 	if self.rank==0:
-	# 		train_history=[]
-	# 		test_history=[]
-
-	# 	if self.rank == 0:
-	# 		if self.model_name=="nanoGPT":
-	# 			print("epoch\tRank\ttest_loss\ttrain_loss\tcons_error\ttest_time\ttrain_time")
-	# 		else:
-	# 			print("epoch\tRank\ttest_acc\ttest_loss\ttrain_acc\ttrain_loss\tcons_error\ttest_time\ttrain_time")
-
-	# 	if self.track:
-	# 		verbose = True
-
-	# 	while self.epoch < self.epochs:
-	# 		# Increment our epoch and set the model to train.
-	# 		self.epoch += 1
-	# 		self.model.train()
-
-	# 		# If verbose, print our stats.
-	# 		if self.track:
-	# 			test_loss,test_acc,test_time = self.tracker.evaluate(loader="test")
-	# 			if isinstance(self.optim, list):
-	# 				cons_error = 0.0  # Placeholder for dual optimizer
-	# 			else:
-	# 				cons_error = self.tracker.compute_cons_error(self.comm_set,self.optim)
-	# 			train_loss,train_acc,train_time = self.tracker.evaluate(loader="train")
-	# 			if verbose:
-	# 				if self.model_name=="nanoGPT":
-	# 					#print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(self.epoch-1,self.rank,test_loss,train_loss,cons_error,test_time,train_time))
-	# 					if (self.rank==0):
-	# 						print("{}\t{}\t{}\t{}".format(self.epoch-1,self.rank,test_loss,train_loss))
-	# 						train_history.append(train_loss)	
-	# 						test_history.append(test_loss)
-						
-	# 				else:
-	# 					print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(self.epoch-1,self.rank,test_acc,test_loss,train_acc,train_loss,cons_error,test_time,train_time))
-	# 		loss = None
-
-	# 		# Perform training for our batches.
-	# 		for _, (data,target) in enumerate(self.train_loader):
-	# 			# Do the standard training stuff.
-	# 			data,target = data.to(self.device), target.to(self.device)
-	# 			self.model.zero_grad()
-	# 			if self.model_name=="nanoGPT":
-	# 				output,loss = self.model(data,target)
-	# 			else:
-	# 				output = self.model(data)
-	# 				loss = self.loss_fcn(output, target)
-	# 			loss.backward()
-				
-	# 			# Perform step.
-	# 			if isinstance(self.optim, list):
-	# 				for opt in self.optim:
-	# 					#print("\n In right optimizer loop")
-	# 					opt.step()
-	# 			else:
-	# 				self.optim.step()
-
-	# 		# Set up a barrier at the end of the epoch.
-	# 		comm = self.optim[0].COMM if isinstance(self.optim, list) else self.optim.COMM
-	# 		comm.Barrier()
-
-	# 		losses = torch.zeros(self.epochs)
-	# 		for k in range(self.epochs):
-	# 			losses[k] = loss.item()
-	# 		losses_mean = losses.mean()
-	# 		if losses_mean < self.best_val_loss:
-	# 			self.best_val_loss = losses_mean
-	# 			if self.epoch % 25==0:
-	# 				if isinstance(self.optim, list):
-	# 					optimizer_state = [opt.state_dict() for opt in self.optim]
-	# 				else:
-	# 					optimizer_state = self.optim.state_dict()
-	# 				checkpoint = {
-	# 					'model': self.model.state_dict(),
-	# 					'optimizer': optimizer_state,
-	# 					'iter_num': self.epoch,
-	# 					'best_val_loss': self.best_val_loss
-	# 				}
-	# 				if self.model_name == "nanoGPT":
-	# 					checkpoint.update({'model_args':self.model.model_args, 'config': self.model.config})
-	# 				os.makedirs(self.model.out_dir, exist_ok=True)
-	# 				torch.save(checkpoint, os.path.join(self.model.out_dir, 'chk_'+output_file+'_Epoch_'+str(self.epoch)+'.pt'))
-	# 				print(f"Checkpoint saved at {self.model.out_dir}")
-
-
-			'''
+	'''
 	Performs training for the defined number of epochs.
 	'''
-	def train(self, output_file="default", verbose=False):
+	def train(self,output_file="default",verbose=False):
+		if self.rank==0:
+			train_history=[]
+			test_history=[]
+
 		if self.rank == 0:
-			train_history = []
-			test_history = []
-			# Added 'Batch' column to the header
-			if self.model_name == "nanoGPT":
-				print("Epoch\tBatch\tRank\tTest_Loss\tTrain_Loss")
+			if self.model_name=="nanoGPT":
+				print("epoch\tRank\ttest_loss\ttrain_loss\tcons_error\ttest_time\ttrain_time")
 			else:
-				print("Epoch\tBatch\tRank\tTest_Acc\tTest_Loss\tTrain_Acc\tTrain_Loss")
+				print("epoch\tRank\ttest_acc\ttest_loss\ttrain_acc\ttrain_loss\tcons_error\ttest_time\ttrain_time")
 
 		if self.track:
 			verbose = True
-
-		# Initialize test_loss to 0 or initial eval so it's defined for the first print
-		test_loss = 0.0
-		if self.track:
-			# Initial baseline check (optional, can be removed if not needed)
-			test_loss, _, _ = self.tracker.evaluate(loader="test")
 
 		while self.epoch < self.epochs:
 			# Increment our epoch and set the model to train.
 			self.epoch += 1
 			self.model.train()
 
+			# If verbose, print our stats.
+			if self.track:
+				test_loss,test_acc,test_time = self.tracker.evaluate(loader="test")
+				if isinstance(self.optim, list):
+					cons_error = 0.0  # Placeholder for dual optimizer
+				else:
+					cons_error = self.tracker.compute_cons_error(self.comm_set,self.optim)
+				train_loss,train_acc,train_time = self.tracker.evaluate(loader="train")
+				if verbose:
+					if self.model_name=="nanoGPT":
+						#print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(self.epoch-1,self.rank,test_loss,train_loss,cons_error,test_time,train_time))
+						if (self.rank==0):
+							print("{}\t{}\t{}\t{}".format(self.epoch-1,self.rank,test_loss,train_loss))
+							train_history.append(train_loss)	
+							test_history.append(test_loss)
+						
+					else:
+						print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(self.epoch-1,self.rank,test_acc,test_loss,train_acc,train_loss,cons_error,test_time,train_time))
+			loss = None
+
 			# Perform training for our batches.
-			# Changed loop to capture batch_idx
-			for batch_idx, (data, target) in enumerate(self.train_loader):
-				data, target = data.to(self.device), target.to(self.device)
+			for _, (data,target) in enumerate(self.train_loader):
+				# Do the standard training stuff.
+				data,target = data.to(self.device), target.to(self.device)
 				self.model.zero_grad()
-				
-				# Forward pass
-				if self.model_name == "nanoGPT":
-					output, loss = self.model(data, target)
+				if self.model_name=="nanoGPT":
+					output,loss = self.model(data,target)
 				else:
 					output = self.model(data)
 					loss = self.loss_fcn(output, target)
-				
 				loss.backward()
 				
 				# Perform step.
 				if isinstance(self.optim, list):
 					for opt in self.optim:
+						#print("\n In right optimizer loop")
 						opt.step()
 				else:
 					self.optim.step()
 
-				# --- LOGGING MOVED INSIDE THE BATCH LOOP ---
-				if self.track and verbose and self.rank == 0:
-					# We use loss.item() for current batch loss (FAST) 
-					# instead of self.tracker.evaluate (SLOW)
-					current_train_loss = loss.item() 
-					
-					# NOTE: Calculating full test_loss every batch is extremely expensive.
-					# We print the last known test_loss or you can re-evaluate periodically:
-					# if batch_idx % 100 == 0: test_loss,_,_ = self.tracker.evaluate(loader="test")
-					
-					if self.model_name == "nanoGPT":
-						print("{}\t{}\t{}\t{:.4f}\t{:.4f}".format(
-							self.epoch, batch_idx, self.rank, test_loss, current_train_loss))
-						
-						# Append history per batch
-						train_history.append(current_train_loss)
-						test_history.append(test_loss)
-					else:
-						# For other models, you might want accuracy. 
-						# Note: calculating accuracy per batch requires extra logic not shown here.
-						print("{}\t{}\t{}\t{:.4f}\t{:.4f}".format(
-							self.epoch, batch_idx, self.rank, test_loss, current_train_loss))
-
-			# --- End of Batch Loop ---
-
 			# Set up a barrier at the end of the epoch.
 			comm = self.optim[0].COMM if isinstance(self.optim, list) else self.optim.COMM
 			comm.Barrier()
-			
-			# Update test_loss at the end of the epoch for the next loop's display
-			if self.track:
-				test_loss, test_acc, test_time = self.tracker.evaluate(loader="test")
 
-			# Checkpoint logic (remains at end of epoch)
 			losses = torch.zeros(self.epochs)
-			# Note: logic below assumes 'loss' is the last batch's loss. 
-			# For better accuracy, you might want to average the epoch losses.
-			if self.rank == 0:
-				current_val_loss = test_loss # Use the actual test loss for checkpointing
-				if current_val_loss < self.best_val_loss:
-					self.best_val_loss = current_val_loss
-					if self.epoch % 25 == 0:
-						if isinstance(self.optim, list):
-							optimizer_state = [opt.state_dict() for opt in self.optim]
-						else:
-							optimizer_state = self.optim.state_dict()
-						checkpoint = {
-							'model': self.model.state_dict(),
-							'optimizer': optimizer_state,
-							'iter_num': self.epoch,
-							'best_val_loss': self.best_val_loss
-						}
-						if self.model_name == "nanoGPT":
-							checkpoint.update({'model_args': self.model.model_args, 'config': self.model.config})
-						os.makedirs(self.model.out_dir, exist_ok=True)
-						torch.save(checkpoint, os.path.join(self.model.out_dir, 'chk_'+output_file+'_Epoch_'+str(self.epoch)+'.pt'))
-						print(f"Checkpoint saved at {self.model.out_dir}")
+			for k in range(self.epochs):
+				losses[k] = loss.item()
+			losses_mean = losses.mean()
+			if losses_mean < self.best_val_loss:
+				self.best_val_loss = losses_mean
+				if self.epoch % 25==0:
+					if isinstance(self.optim, list):
+						optimizer_state = [opt.state_dict() for opt in self.optim]
+					else:
+						optimizer_state = self.optim.state_dict()
+					checkpoint = {
+						'model': self.model.state_dict(),
+						'optimizer': optimizer_state,
+						'iter_num': self.epoch,
+						'best_val_loss': self.best_val_loss
+					}
+					if self.model_name == "nanoGPT":
+						checkpoint.update({'model_args':self.model.model_args, 'config': self.model.config})
+					os.makedirs(self.model.out_dir, exist_ok=True)
+					torch.save(checkpoint, os.path.join(self.model.out_dir, 'chk_'+output_file+'_Epoch_'+str(self.epoch)+'.pt'))
+					print(f"Checkpoint saved at {self.model.out_dir}")
+
+
+	# 		'''
+	# Performs training for the defined number of epochs.
+	# '''
+	# def train(self, output_file="default", verbose=False):
+	# 	if self.rank == 0:
+	# 		train_history = []
+	# 		test_history = []
+	# 		# Added 'Batch' column to the header
+	# 		if self.model_name == "nanoGPT":
+	# 			print("Epoch\tBatch\tRank\tTest_Loss\tTrain_Loss")
+	# 		else:
+	# 			print("Epoch\tBatch\tRank\tTest_Acc\tTest_Loss\tTrain_Acc\tTrain_Loss")
+
+	# 	if self.track:
+	# 		verbose = True
+
+	# 	# Initialize test_loss to 0 or initial eval so it's defined for the first print
+	# 	test_loss = 0.0
+	# 	if self.track:
+	# 		# Initial baseline check (optional, can be removed if not needed)
+	# 		test_loss, _, _ = self.tracker.evaluate(loader="test")
+
+	# 	while self.epoch < self.epochs:
+	# 		# Increment our epoch and set the model to train.
+	# 		self.epoch += 1
+	# 		self.model.train()
+
+	# 		# Perform training for our batches.
+	# 		# Changed loop to capture batch_idx
+	# 		for batch_idx, (data, target) in enumerate(self.train_loader):
+	# 			data, target = data.to(self.device), target.to(self.device)
+	# 			self.model.zero_grad()
+				
+	# 			# Forward pass
+	# 			if self.model_name == "nanoGPT":
+	# 				output, loss = self.model(data, target)
+	# 			else:
+	# 				output = self.model(data)
+	# 				loss = self.loss_fcn(output, target)
+				
+	# 			loss.backward()
+				
+	# 			# Perform step.
+	# 			if isinstance(self.optim, list):
+	# 				for opt in self.optim:
+	# 					opt.step()
+	# 			else:
+	# 				self.optim.step()
+
+	# 			# --- LOGGING MOVED INSIDE THE BATCH LOOP ---
+	# 			if self.track and verbose and self.rank == 0:
+	# 				# We use loss.item() for current batch loss (FAST) 
+	# 				# instead of self.tracker.evaluate (SLOW)
+	# 				current_train_loss = loss.item() 
+					
+	# 				# NOTE: Calculating full test_loss every batch is extremely expensive.
+	# 				# We print the last known test_loss or you can re-evaluate periodically:
+	# 				# if batch_idx % 100 == 0: test_loss,_,_ = self.tracker.evaluate(loader="test")
+					
+	# 				if self.model_name == "nanoGPT":
+	# 					print("{}\t{}\t{}\t{:.4f}\t{:.4f}".format(
+	# 						self.epoch, batch_idx, self.rank, test_loss, current_train_loss))
+						
+	# 					# Append history per batch
+	# 					train_history.append(current_train_loss)
+	# 					test_history.append(test_loss)
+	# 				else:
+	# 					# For other models, you might want accuracy. 
+	# 					# Note: calculating accuracy per batch requires extra logic not shown here.
+	# 					print("{}\t{}\t{}\t{:.4f}\t{:.4f}".format(
+	# 						self.epoch, batch_idx, self.rank, test_loss, current_train_loss))
+
+	# 		# --- End of Batch Loop ---
+
+	# 		# Set up a barrier at the end of the epoch.
+	# 		comm = self.optim[0].COMM if isinstance(self.optim, list) else self.optim.COMM
+	# 		comm.Barrier()
+			
+	# 		# Update test_loss at the end of the epoch for the next loop's display
+	# 		if self.track:
+	# 			test_loss, test_acc, test_time = self.tracker.evaluate(loader="test")
+
+	# 		# Checkpoint logic (remains at end of epoch)
+	# 		losses = torch.zeros(self.epochs)
+	# 		# Note: logic below assumes 'loss' is the last batch's loss. 
+	# 		# For better accuracy, you might want to average the epoch losses.
+	# 		if self.rank == 0:
+	# 			current_val_loss = test_loss # Use the actual test loss for checkpointing
+	# 			if current_val_loss < self.best_val_loss:
+	# 				self.best_val_loss = current_val_loss
+	# 				if self.epoch % 25 == 0:
+	# 					if isinstance(self.optim, list):
+	# 						optimizer_state = [opt.state_dict() for opt in self.optim]
+	# 					else:
+	# 						optimizer_state = self.optim.state_dict()
+	# 					checkpoint = {
+	# 						'model': self.model.state_dict(),
+	# 						'optimizer': optimizer_state,
+	# 						'iter_num': self.epoch,
+	# 						'best_val_loss': self.best_val_loss
+	# 					}
+	# 					if self.model_name == "nanoGPT":
+	# 						checkpoint.update({'model_args': self.model.model_args, 'config': self.model.config})
+	# 					os.makedirs(self.model.out_dir, exist_ok=True)
+	# 					torch.save(checkpoint, os.path.join(self.model.out_dir, 'chk_'+output_file+'_Epoch_'+str(self.epoch)+'.pt'))
+	# 					print(f"Checkpoint saved at {self.model.out_dir}")
 
 					
 		# Write to Matlab file
